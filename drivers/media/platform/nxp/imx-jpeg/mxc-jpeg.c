ca1bb79638f78774d7bc4447c6417a145011da41
Ming Qian <ming.qian@nxp.com>
2025-03-20 17:20:39 +0900

    LF-5615: media: imx-jpeg: Check decoding is ongoing for motion-jpeg
    To support decoding motion-jpeg without DHT, driver will try to decode a
    pattern jpeg before actual jpeg frame by use of linked descriptors
    (This is called "repeat mode"), then the DHT in the pattern jpeg can be
    used for decoding the motion-jpeg.
    
    In other words, 2 frame done interrupts will be triggered, driver will
    ignore the first interrupt, and wait for the second interrupt.
    If the resolution is small, and the 2 interrupts may be too close,
    when driver is handling the first interrupt, two frames are done, then
    driver will fail to wait for the second interrupt.
    
    In such case, driver can check whether the decoding is still ongoing,
    if not, just done the current decoding.
    
    Signed-off-by: Ming Qian <ming.qian@nxp.com>
    Reviewed-by: Zhou Peng <eagle.zhou@nxp.com>

--- drivers/media/platform/nxp/imx-jpeg/mxc-jpeg.c
+++ drivers/media/platform/nxp/imx-jpeg/mxc-jpeg.c
@@ -922,6 +922,23 @@ static u32 mxc_jpeg_get_plane_size(struct mxc_jpeg_q_data *q_data, u32 plane_no)
 	return size;
 }
 
+static bool mxc_dec_is_ongoing(struct mxc_jpeg_ctx *ctx)
+{
+	struct mxc_jpeg_dev *jpeg = ctx->mxc_jpeg;
+	u32 curr_desc;
+	u32 slot_status;
+
+	slot_status = readl(jpeg->base_reg + MXC_SLOT_OFFSET(ctx->slot, SLOT_STATUS));
+	curr_desc = readl(jpeg->base_reg + MXC_SLOT_OFFSET(ctx->slot, SLOT_CUR_DESCPT_PTR));
+
+	if (curr_desc == jpeg->slot_data.cfg_desc_handle)
+		return true;
+	if (slot_status & SLOT_STATUS_ONGOING)
+		return true;
+
+	return false;
+}
+
 static irqreturn_t mxc_jpeg_dec_irq(int irq, void *priv)
 {
 	struct mxc_jpeg_dev *jpeg = priv;
@@ -991,7 +1008,8 @@ static irqreturn_t mxc_jpeg_dec_irq(int irq, void *priv)
 		mxc_jpeg_enc_mode_go(dev, reg, mxc_jpeg_is_extended_sequential(q_data->fmt));
 		goto job_unlock;
 	}
-	if (jpeg->mode == MXC_JPEG_DECODE && jpeg_src_buf->dht_needed) {
+	if (jpeg->mode == MXC_JPEG_DECODE && jpeg_src_buf->dht_needed &&
+	    mxc_dec_is_ongoing(ctx)) {
 		jpeg_src_buf->dht_needed = false;
 		dev_dbg(dev, "Decoder DHT cfg finished. Start decoding...\n");
 		goto job_unlock;
