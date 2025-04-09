analogdevicesinc/linux

commit fd469c026e841aa64fd73b227795bb3e9482668a

LF-14099-2 PCI: imx6: Toggle the cold reset for i.MX95 PCIe
Beside the power-on reset, add the cold reset toggle for i.MX95 PCIe.

Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
Reviewed-by: Frank Li <Frank.Li@nxp.com>

drivers/pci/controller/dwc/pci-imx6.c
@@ -72,6 +72,9 @@
 #define IMX95_SID_MASK				GENMASK(5, 0)
 #define IMX95_MAX_LUT				32
 
+#define IMX95_PCIE_RST_CTRL			0x3010
+#define IMX95_PCIE_COLD_RST			BIT(0)
+
 #define to_imx_pcie(x)	dev_get_drvdata((x)->dev)
 
 enum imx_pcie_variants {
@@ -859,6 +862,31 @@ static int imx7d_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 		dev_warn(dev, "Unable to apply ERR010728 workaround. DT missing fsl,imx7d-pcie-phy phandle ?\n");
 	}
 	imx7d_pcie_wait_for_phy_pll_lock(imx_pcie);
+	return 0;
+}
+
+static int imx95_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
+{
+	if (assert) {
+		/*
+		 * From i.MX95 PCIe PHY perspective, the COLD reset toggle
+		 * should be complete after power-up by the following sequence.
+		 *                 > 10us(at power-up)
+		 *                 > 10ns(warm reset)
+		 *               |<------------>|
+		 *                ______________
+		 * phy_reset ____/              \________________
+		 *                                   ____________
+		 * ref_clk_en_______________________/
+		 * Toggle COLD reset aligned with this sequence for i.MX95 PCIe.
+		 */
+		regmap_update_bits(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
+				   IMX95_PCIE_COLD_RST, IMX95_PCIE_COLD_RST);
+		udelay(10);
+		regmap_update_bits(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
+				   IMX95_PCIE_COLD_RST, 0);
+	}
+
 	return 0;
 }
 
@@ -1934,6 +1962,7 @@ static const struct imx_pcie_drvdata drvdata[] = {
 		.mode_mask[0] = IMX95_PCIE_DEVICE_TYPE,
 		.init_phy = imx95_pcie_init_phy,
 		.enable_ref_clk = imx95_pcie_enable_ref_clk,
+		.core_reset = imx95_pcie_core_reset,
 	},
 	[IMX6Q_EP] = {
 		.variant = IMX6Q_EP,

atmark-techno/linux-6.12-at

commit e3fd471a18cda389843fb95a7f0be5b2774aec23

LF-15033 PCI: imx6: Add delay between cold reset and clock enable for i.MX95 PCIe
The ref clock comes after reset, internal synced reset time is too short,
cannot meet the requirement. Add one ~10us delay after cold reset.

Make sure the write to IMX95_PCIE_RST_CTRL is flushed to the hardware by
doing a read. Otherwise, there is no guarantee that the write has reached
the hardware before udelay().

Fixes: fd469c026e84 ("LF-14099-2 PCI: imx6: Toggle the cold reset for i.MX95 PCIe")
Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
Reviewed-by: Frank Li <Frank.Li@nxp.com>

drivers/pci/controller/dwc/pci-imx6.c
@@ -897,6 +897,8 @@ static int imx7d_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 
 static int imx95_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 {
+	u32 val;
+
 	if (assert) {
 		/*
 		 * From i.MX95 PCIe PHY perspective, the COLD reset toggle
@@ -912,9 +914,19 @@ static int imx95_pcie_core_reset(struct imx_pcie *imx_pcie, bool assert)
 		 */
 		regmap_update_bits(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
 				   IMX95_PCIE_COLD_RST, IMX95_PCIE_COLD_RST);
+		/*
+		 * Make sure the write to IMX95_PCIE_RST_CTRL is flushed to the
+		 * hardware by doing a read. Otherwise, there is no guarantee
+		 * that the write has reached the hardware before udelay().
+		 */
+		regmap_read_bypassed(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
+				     &val);
+		udelay(15);
+		regmap_clear_bits(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
+				  IMX95_PCIE_COLD_RST);
+		regmap_read_bypassed(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
+				     &val);
 		udelay(10);
-		regmap_update_bits(imx_pcie->iomuxc_gpr, IMX95_PCIE_RST_CTRL,
-				   IMX95_PCIE_COLD_RST, 0);
 	}
 
 	return 0;