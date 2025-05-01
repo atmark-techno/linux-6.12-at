074c8c3017af9cbcb997584ee872d4ec2c36d1f7
Daisuke Mizobuchi <mizo@atmark-techno.com>
2025-05-01 15:59:09 +0900

    ASoC: imx-pcm-rpmsg: add PCM_INIT command
    In atmark-techno m33 firmware, the device will not be initialized
    until the PCM_INIT command is executed.
    
    Signed-off-by: Daisuke Mizobuchi <mizo@atmark-techno.com>
    (cherry picked from commit 6838055f5bc74cf84d3e7c2f59ff9d399e3e4e66 @ https://github.com/atmark-techno/linux-5.10-at)

--- sound/soc/fsl/imx-pcm-rpmsg.c
+++ sound/soc/fsl/imx-pcm-rpmsg.c
@@ -598,6 +598,8 @@ static int imx_rpmsg_pcm_new(struct snd_soc_component *component,
 	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	struct fsl_rpmsg *rpmsg = dev_get_drvdata(cpu_dai->dev);
 	struct snd_pcm_substream *substream;
+	struct rpmsg_info *info = dev_get_drvdata(component->dev);
+	struct rpmsg_msg *msg;
 	int ret;
 
 	ret = dma_coerce_mask_and_coherent(card->dev, DMA_BIT_MASK(32));
@@ -618,6 +620,10 @@ static int imx_rpmsg_pcm_new(struct snd_soc_component *component,
 		if (ret < 0)
 			return ret;
 	}
+
+	msg = &info->msg[PCM_INIT];
+	msg->s_msg.header.cmd = PCM_INIT;
+	info->send_message(msg, info);
 
 	return ret;
 }
