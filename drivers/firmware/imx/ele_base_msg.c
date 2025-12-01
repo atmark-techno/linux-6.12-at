atmark-techno/linux-5.10-at

firmware: imx: ele_fw: implement secureboot-related API
implement get_events and forward_lifecycle for secureboot:
- get_events lists current secureboot violations and is required
to check it is safe to close
- forward_lifecycle allows closing the device

Change-Id: Iae82d653c29cdfff5e538c37b7880f1a0ebdd981
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

drivers/firmware/imx/ele_base_msg.c
@@ -154,6 +154,112 @@ exit:
 }
 EXPORT_SYMBOL_GPL(ele_write_fuse);
 
+int ele_get_events(struct device *dev, u32 *events, u32 *events_count)
+{
+	struct ele_mu_priv *priv = dev_get_drvdata(dev);
+	unsigned int status, ind;
+	u16 actual_count, i;
+	int ret;
+
+	ret = imx_se_alloc_tx_rx_buf(priv);
+	if (ret)
+		return ret;
+
+	ret = plat_fill_cmd_msg_hdr(priv,
+				    (struct mu_hdr *)&priv->tx_msg->header,
+				    ELE_GET_EVENTS_REQ,
+				    ELE_GET_EVENTS_REQ_MSG_SZ,
+				    true);
+	if (ret) {
+		pr_err("Error: plat_fill_cmd_msg_hdr failed.\n");
+		goto exit;
+	}
+
+	ret = imx_ele_msg_send_rcv(priv);
+	if (ret < 0)
+		goto exit;
+
+	ret  = validate_rsp_hdr(priv,
+				priv->rx_msg->header,
+				ELE_GET_EVENTS_REQ,
+				ELE_GET_EVENTS_RSP_MSG_SZ,
+				true);
+	if (ret)
+		goto exit;
+
+	status = RES_STATUS(priv->rx_msg->data[0]);
+	ind = RES_IND(priv->rx_msg->data[0]);
+	if (status != priv->success_tag) {
+		dev_err(dev, "Command Id[%d], Status=0x%x, Indicator=0x%x",
+			ELE_GET_EVENTS_REQ, status, ind);
+		ret = -1;
+		goto exit;
+	}
+
+	actual_count = priv->rx_msg->data[1] & 0xffff;
+	if (actual_count > ELE_GET_EVENTS_MAX_COUNT)
+		actual_count = ELE_GET_EVENTS_MAX_COUNT;
+	if (actual_count > *events_count)
+		actual_count = *events_count;
+
+	for (i = 0; i < actual_count; i++)
+		events[i] = priv->rx_msg->data[i + 2];
+	*events_count = actual_count;
+
+exit:
+	imx_se_free_tx_rx_buf(priv);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(ele_get_events);
+
+int ele_forward_lifecycle(struct device *dev, u16 life_cycle)
+{
+	struct ele_mu_priv *priv = dev_get_drvdata(dev);
+	unsigned int status, ind;
+	int ret;
+
+	ret = imx_se_alloc_tx_rx_buf(priv);
+	if (ret)
+		return ret;
+
+	ret = plat_fill_cmd_msg_hdr(priv,
+				    (struct mu_hdr *)&priv->tx_msg->header,
+				    ELE_FWD_LIFECYCLE_UP_REQ,
+				    ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ,
+				    true);
+	if (ret) {
+		pr_err("Error: plat_fill_cmd_msg_hdr failed.\n");
+		goto exit;
+	}
+
+	*((u16*)priv->tx_msg->data) = life_cycle;
+
+	ret = imx_ele_msg_send_rcv(priv);
+	if (ret < 0)
+		goto exit;
+
+	ret  = validate_rsp_hdr(priv,
+				priv->rx_msg->header,
+				ELE_FWD_LIFECYCLE_UP_REQ,
+				ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ,
+				true);
+	if (ret)
+		goto exit;
+
+	status = RES_STATUS(priv->rx_msg->data[0]);
+	ind = RES_IND(priv->rx_msg->data[0]);
+	if (status != priv->success_tag) {
+		dev_err(dev, "Command Id[%d], Status=0x%x, Indicator=0x%x",
+			ELE_FWD_LIFECYCLE_UP_REQ, status, ind);
+		ret = -1;
+	}
+
+exit:
+	imx_se_free_tx_rx_buf(priv);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(ele_forward_lifecycle);
+
 int ele_ping(struct device *dev)
 {
 	struct ele_mu_priv *priv = dev_get_drvdata(dev);

include/linux/firmware/imx/ele_base_msg.h
@@ -68,6 +68,17 @@
 #define OTP_UNIQ_ID			0x01
 #define OTFAD_CONFIG			0x2
 
+#define ELE_FWD_LIFECYCLE_UP_REQ	0x95
+#define ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ	0x08
+#define ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ	0x08
+
+#define ELE_GET_EVENTS_REQ		0xA2
+#define ELE_GET_EVENTS_REQ_MSG_SZ	0x04
+#define ELE_GET_EVENTS_RSP_MSG_SZ	0x30
+/* - 3 for header, status and count itself */
+#define ELE_GET_EVENTS_MAX_COUNT \
+	((ELE_GET_EVENTS_RSP_MSG_SZ >> 2) - 3)
+
 #define ELE_GET_STATE			0xB2
 #define ELE_GET_STATE_REQ_SZ		0x04
 #define ELE_GET_STATE_RSP_SZ		0x10
@@ -88,6 +99,8 @@ int ele_service_swap(struct device *dev,
 		     u32 addr_size, u16 flag);
 int ele_get_v2x_fw_state(struct device *dev, uint32_t *state);
 int ele_write_fuse(struct device *dev, uint16_t fuse_index, u32 value, bool block);
+int ele_get_events(struct device *dev, u32 *events, u32 *events_count);
+int ele_forward_lifecycle(struct device *dev, u16 life_cycle);
 
 int read_common_fuse(struct device *dev,
 		     uint16_t fuse_id, u32 *value);

atmark-techno/linux-6.12-at

(cherry picked from commit 4bfa8e1a83adf593c84b063466f64644fef692cb)
Signed-off-by: Daisuke Mizobuchi <mizo@atmark-techno.com>

drivers/firmware/imx/ele_base_msg.c
@@ -224,6 +224,136 @@ exit:
 	return ret;
 }
 
+int ele_get_events(struct se_if_priv *priv, u32 *events, u32 *events_count)
+{
+	struct se_api_msg *tx_msg __free(kfree) = NULL;
+	struct se_api_msg *rx_msg __free(kfree) = NULL;
+	u16 actual_count, i;
+	int ret;
+
+	if (!priv) {
+		ret = -EINVAL;
+		goto exit;
+	}
+
+	tx_msg = kzalloc(ELE_GET_EVENTS_REQ_MSG_SZ, GFP_KERNEL);
+	if (!tx_msg) {
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+	rx_msg = kzalloc(ELE_GET_EVENTS_RSP_MSG_SZ, GFP_KERNEL);
+	if (!rx_msg) {
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+	ret = se_fill_cmd_msg_hdr(priv,
+				  (struct se_msg_hdr *)&tx_msg->header,
+				  ELE_GET_EVENTS_REQ,
+				  ELE_GET_EVENTS_REQ_MSG_SZ,
+				  true);
+	if (ret) {
+		dev_err(priv->dev, "Error: se_fill_cmd_msg_hdr failed.\n");
+		goto exit;
+	}
+
+	ret = ele_msg_send_rcv(priv->priv_dev_ctx,
+			       tx_msg,
+			       ELE_GET_EVENTS_REQ_MSG_SZ,
+			       rx_msg,
+			       ELE_GET_EVENTS_RSP_MSG_SZ);
+	if (ret < 0)
+		goto exit;
+
+	ret = se_val_rsp_hdr_n_status(priv,
+				      rx_msg,
+				      ELE_GET_EVENTS_REQ,
+				      ELE_GET_EVENTS_RSP_MSG_SZ,
+				      true);
+	if (ret)
+		goto exit;
+
+	actual_count = rx_msg->data[1] & 0xffff;
+	if (actual_count > ELE_GET_EVENTS_MAX_COUNT)
+		actual_count = ELE_GET_EVENTS_MAX_COUNT;
+	if (actual_count > *events_count)
+		actual_count = *events_count;
+
+	for (i = 0; i < actual_count; i++)
+		events[i] = rx_msg->data[i + 2];
+	*events_count = actual_count;
+
+exit:
+	return ret;
+}
+
+int imx_se_get_events(void *se_if_data, u32 *events, u32 *events_count)
+{
+	return ele_get_events((struct se_if_priv *)se_if_data, events, events_count);
+}
+EXPORT_SYMBOL_GPL(imx_se_get_events);
+
+int ele_forward_lifecycle(struct se_if_priv *priv, u16 life_cycle)
+{
+	struct se_api_msg *tx_msg __free(kfree) = NULL;
+	struct se_api_msg *rx_msg __free(kfree) = NULL;
+	int ret;
+
+	if (!priv) {
+		ret = -EINVAL;
+		goto exit;
+	}
+
+	tx_msg = kzalloc(ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ, GFP_KERNEL);
+	if (!tx_msg) {
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+	rx_msg = kzalloc(ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ, GFP_KERNEL);
+	if (!rx_msg) {
+		ret = -ENOMEM;
+		goto exit;
+	}
+
+
+	ret = se_fill_cmd_msg_hdr(priv,
+				  (struct se_msg_hdr *)&tx_msg->header,
+				  ELE_FWD_LIFECYCLE_UP_REQ,
+				  ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ,
+				  true);
+	if (ret) {
+		dev_err(priv->dev, "Error: se_fill_cmd_msg_hdr failed.\n");
+		goto exit;
+	}
+
+	*((u16*)tx_msg->data) = life_cycle;
+
+	ret = ele_msg_send_rcv(priv->priv_dev_ctx,
+			       tx_msg,
+			       ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ,
+			       rx_msg,
+			       ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ);
+	if (ret < 0)
+		goto exit;
+
+	ret = se_val_rsp_hdr_n_status(priv,
+				      rx_msg,
+				      ELE_FWD_LIFECYCLE_UP_REQ,
+				      ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ,
+				      true);
+
+exit:
+	return ret;
+}
+
+int imx_se_forward_lifecycle(void *se_if_data, u16 life_cycle)
+{
+	return ele_forward_lifecycle((struct se_if_priv *)se_if_data, life_cycle);
+}
+EXPORT_SYMBOL_GPL(imx_se_forward_lifecycle);
+
 int ele_fw_authenticate(struct se_if_priv *priv, phys_addr_t addr)
 {
 	struct se_api_msg *tx_msg __free(kfree) = NULL;

drivers/firmware/imx/ele_base_msg.h
@@ -111,6 +111,17 @@ struct ele_dev_info {
 #define ELE_VOLT_CHANGE_REQ_MSG_SZ			0x4
 #define ELE_VOLT_CHANGE_RSP_MSG_SZ			0x8
 
+#define ELE_FWD_LIFECYCLE_UP_REQ       0x95
+#define ELE_FWD_LIFECYCLE_UP_REQ_MSG_SZ        0x08
+#define ELE_FWD_LIFECYCLE_UP_RSP_MSG_SZ        0x08
+
+#define ELE_GET_EVENTS_REQ             0xA2
+#define ELE_GET_EVENTS_REQ_MSG_SZ      0x04
+#define ELE_GET_EVENTS_RSP_MSG_SZ      0x30
+/* - 3 for header, status and count itself */
+#define ELE_GET_EVENTS_MAX_COUNT \
+	((ELE_GET_EVENTS_RSP_MSG_SZ >> 2) - 3)
+
 #define ELE_GET_STATE			0xB2
 #define ELE_GET_STATE_REQ_SZ		0x04
 #define ELE_GET_STATE_RSP_SZ		0x10
@@ -142,4 +153,6 @@ int ele_v2x_fw_authenticate(struct se_if_priv *priv, phys_addr_t addr);
 int ele_debug_dump(struct se_if_priv *priv);
 int ele_get_fw_version(struct se_if_priv *priv, u32 *fw_ver_word,
 		       u32 *commit_sha1);
+int ele_get_events(struct se_if_priv *priv, u32 *events, u32 *events_count);
+int ele_forward_lifecycle(struct se_if_priv *priv, u16 life_cycle);
 #endif